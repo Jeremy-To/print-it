dafba20c8ab19ef7b3f0988a6d4f18dc4f78d764
jeremy-to <jeremy_to@outlook.fr>
2023-06-10 13:22:52 +0200

    add a space

--- assets/script.js
+++ assets/script.js
@@ -51,7 +51,6 @@ document.addEventListener('DOMContentLoaded', function () {
 		dots[index].classList.add('dot_selected');
 		bannerText.innerHTML = slides[index].tagLine;
 	}
-
 	function slideLeft() {
 		activeIndex = activeIndex > 0 ? activeIndex - 1 : slides.length - 1;
 		changeSlide(activeIndex);
